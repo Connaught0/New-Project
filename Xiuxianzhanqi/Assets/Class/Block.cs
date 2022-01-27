49af94701bd627635a970d5e900c141431ea5ed9
Con <995569113>
2022-01-27 23:23:10 +0800

    Try to add some class and design the Astar algorithm

---
+++ Xiuxianzhanqi/Assets/Class/Block.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block
+{
+    int height;
+    int x;
+    int y;
+    Block right;
+    Block left;
+    Block up;
+    Block down;
+    Object ob;
+    Vector3 position;
+    public Block()
+    {
+
+    }
+    public Block(int _x,int _y)
+    {
+        this.x = _x;
+        this.y = _y;
+    }
+    public Block GetNeibour(string position)
+    {
+        switch(position)
+        {
+            case "right":
+                return right;
+                
+            case "left": return left;
+            case "up": return up;
+            case "down": return down;
+        }
+        return null;
+
+    }
+
+}
---
+++ Xiuxianzhanqi/Assets/Script/Contralor.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Contralor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    List<Block> creatMap(int x,int y)
+    {
+        List<Block> Map = new List<Block>(x*y);
+        for(int i=0;i<x;i++)
+        {
+            for(int j=0;j<y;j++)
+            {
+                Map[j + i * x] = new Block(i, j);
+
+            }
+        }
+        return Map;
+    }
+    class AstarBlock
+    {
+        Block block;
+        float Step;//Just like g in the original algotithm
+        float Distance;//Just like h in the original algotithm
+        public List<Block> LeastRoad(Vector2 start,Vector2 end)
+        {
+            List<Block> road = new List<Block>();
+
+            return road;
+        }
+    }
+}
